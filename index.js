799c644e55728b80efabccfeb1d6d940f7d69482
bhanu2507 <bhanureddy@gmail.com>
2016-12-15 11:41:02 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Created by bhanu.mokkala on 12/14/2016.
+ */
+/**
+ * Created by bhanu.mokkala on 11/28/2016.
+ */
+/**
+ * Created by bhanu.mokkala on 11/4/2016.
+ */
+var express   = require("express");
+//var mysql     = require('mysql');
+var path      = require('path');
+
+var app = express();
+/*
+ var server = require('http').Server(app);
+ var io = require('socket.io')(server);
+ */
+//server.listen(80);
+
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+var app = express();
+
+
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.use('/bower_components', express.static(__dirname + '/bower_components/'));
+
+app.get("/",function(req,res){
+
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+
+// Set port
+port = process.env.PORT || 5000;
+
+// Use public directory for static files
+app.use(express.static(__dirname + '/public'));
+// Include the routes module
+//require('./app/routes')(app);
+
+// Your code here
+app.listen(port);
+
+
+
